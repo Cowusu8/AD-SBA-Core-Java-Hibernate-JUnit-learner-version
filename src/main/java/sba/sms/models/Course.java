c0c25a517527690394d32412336e8b21d48fdd1b
Crystal <crystalagyeman23@gmail.com>
2023-01-26 21:25:16 -0600

    Added basic requirements for Course and Student in Models

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,59 @@
 package sba.sms.models;
 
-public class Course {
+import java.util.Objects;
 
+/*
+ * Models requires:
+    - no args constructor
+     - all args constructor
+required args constructor
+setters and getter
+toString (exclude collections to avoid infinite loops)
+override equals and hashcode methods (don't use lombok here)
+helper methods
+ */
+public class Course {
+    int id;
+    String name, instructor;
+
+    public Course() {}
+    public Course(int id, String name, String instructor) {
+        this.name = name;
+        this.id = id;
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", instructor='" + instructor + '\'' +
+                '}';
+    }
+
+    /* required args constructor */
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
+
--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,5 +1,75 @@
 package sba.sms.models;
 
+/*
+ * Models requires:
+    - no args constructor
+     - all args constructor
+required args constructor
+setters and getter
+toString (exclude collections to avoid infinite loops)
+override equals and hashcode methods (don't use lombok here)
+helper methods
+ */
 public class Student {
+    String name, password, email;
+
+    /* no args constructor */
+    public Student() {}
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /* all args constructor */
+    public Student(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    /* required args constructor */
+    /*
+     * public Student(String name, String password, String email) { this.name =
+     * name; this.password = password; this.email = email; }
+     */
 
 }
